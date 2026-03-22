4988c7f80fdba2bb1288b357756b3a5881bd6948
TJ <tj.wang1995@gmail.com>
2026-03-22 23:29:46 +0800

    feat: Set up Tailwind CSS, DaisyUI, and theme switching with Prettier integration and add new utility packages.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <button class="btn" data-set-theme="dark">Dark</button>
+    <button class="btn" data-set-theme="light">Light</button>
+    <button class="btn" data-set-theme="cupcake">Cupcake</button>
+    <button class="btn" data-set-theme="bumblebee">Bumblebee</button>
+    <button class="btn" data-set-theme="emerald">Emerald</button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { themeChange } from "theme-change";
+
+onMounted(() => {
+  themeChange(false);
+});
+</script>
